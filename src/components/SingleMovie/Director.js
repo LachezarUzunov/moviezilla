6badabd31b026d3ce7bb3a47f4260d8202e952e8
Lachezar Uzunov <uzunov_89_lacho@abv.bg>
2022-11-23 10:40:09 +0200

    CSS changes

--- src/components/SingleMovie/Director.js
+++ src/components/SingleMovie/Director.js
@@ -1,13 +1,7 @@
 import React from "react";
-import classes from "./Director.module.css";
 
 const Director = ({ director }) => {
-  return (
-    <div>
-      <h3>Name:</h3>
-      <h4>{director.original_name}</h4>
-    </div>
-  );
+  return <h4>{director.original_name}</h4>;
 };
 
 export default Director;
